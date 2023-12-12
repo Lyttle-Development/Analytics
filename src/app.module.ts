c92b95a7086309f4da19afa704a30ec97cd19d57
Stualyttle Kirry <Stualyttle@users.noreply.github.com>
2023-12-12 20:46:25 +0100

    Move controllers into sub folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ReportModule } from './report/report.module';
-import { RegisterModule } from './register/register.module';
+import { ReportModule } from './controllers/report/report.module';
+import { RegisterModule } from './controllers/register/register.module';
 
 @Module({
   imports: [ReportModule, RegisterModule],
