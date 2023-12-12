cb8cd5f3843d5ff6021cba077e110c887683a295
Stualyttle Kirry <Stualyttle@users.noreply.github.com>
2023-12-12 21:09:22 +0100

    Add Typeorm & Postgress

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,29 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ReportModule } from './controllers/report/report.module';
 import { RegisterModule } from './controllers/register/register.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ReportModule, RegisterModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT),
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        // 1. https://www.google.com/search?q=what+does+synchronize+do+nestjs
+        // - synchronize: Indicates if database schema should be auto-created on every application launch
+        //
+        // 2. https://docs.nestjs.com/techniques/database#:~:text=WARNING,lose%20production%20data.
+        // - Setting "synchronize: true" shouldn't be used in production - otherwise you can lose production data.
+        synchronize: true,
+      }),
+    }),
+    ReportModule, RegisterModule],
   controllers: [AppController],
   providers: [AppService],
 })
