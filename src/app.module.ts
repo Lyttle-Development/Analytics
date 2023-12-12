b6ffdee9fac9142c3168b5d23af6d8b5747e433a
Stualyttle Kirry <Stualyttle@users.noreply.github.com>
2023-12-12 20:13:22 +0100

    Add register endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ReportModule } from './report/report.module';
+import { RegisterModule } from './register/register.module';
 
 @Module({
-  imports: [ReportModule],
+  imports: [ReportModule, RegisterModule],
   controllers: [AppController],
   providers: [AppService],
 })
